982b4042120f2e31b95d70026b1b9c0c0cc6e0a8
smhrd <smhrd@smart>
2024-12-16 14:30:06 +0900

    프로젝트 생성

---
+++ SpringBoot01/src/main/java/com/smhrd/entity/Board.java
@@ -0,0 +1,80 @@
+package com.smhrd.entity;
+
+import lombok.Data;
+
+@Data
+public class Board {
+	
+	private String title;
+	
+	private String writer;
+	
+	private String content;
+	
+	private String img;
+	
+	private String indate;
+	
+	private int count;
+	
+	private int idx;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getIndate() {
+		return indate;
+	}
+
+	public void setIndate(String indate) {
+		this.indate = indate;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	
+
+	
+}
---
+++ SpringBoot01/src/main/java/com/smhrd/entity/Member.java
@@ -0,0 +1,63 @@
+package com.smhrd.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@NoArgsConstructor // 기본생성자****
+@RequiredArgsConstructor // NonNull이 걸린 필드만 초기화
+@AllArgsConstructor // 모든 필드를 초기화하는 생성자
+@Data // getter/setter 등 기본메소드 생성(Lombok)
+public class Member {
+	
+	// DTO(Data Transfer Object)
+	// 계층간 데이터 전송을 위해 사용되는 객체
+	// 여러 데이터를 담을 바구니
+	@NonNull
+	private String email;
+	
+	private String pw;
+	
+	private String tel;
+	
+	private String address;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+
+}
